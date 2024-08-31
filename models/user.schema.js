acc1fb5ce43b6dc1bad2408092b046bbee637ddc
WebDev2105 <choudareddy9842@gmail.com>
2024-08-31 06:10:58 +0530

    userSchema methods

--- models/user.schema.js
+++ models/user.schema.js
@@ -3,6 +3,7 @@ import AuthRoles from "../utils/authRoles";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
+import config from "../config/index.js";
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -39,4 +40,22 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+userSchema.methods = {
+  //compare password
+  comparePassword: async function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+  },
+  getJwtToken: function () {
+    return jwt.sign(
+      {
+        _id: this._id,
+        role: this.role,
+      },
+      config.JWT_SECRET, //this value should come from .env file
+      {
+        expiresIn: config.JWT_EXPIRY, //this value should come from .env file
+      }
+    );
+  },
+};
 export default mongoose.model("User", userSchema);
